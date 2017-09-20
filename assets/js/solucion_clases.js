3777e29fd0e81b1d956703b555bc12c9b91ef787
MelanieOC <melanie.ocharan@gmail.com>
2017-09-20 18:37:21 -0500

    objetos

--- assets/js/solucion_clases.js
+++ assets/js/solucion_clases.js
@@ -1,83 +1,74 @@
 'use strict';
-$(document).ready(eventos);
 
+const aplicacion = {
+    estudiantes:[],
 
-let estudiantes = [];
-class Estudiante {
-    constructor(nombre, puntosTecnicos, puntosHSE){
-        this.nombre = nombre;
-        this.puntosTecnicos = puntosTecnicos;
-        this.puntosHSE = puntosHSE;
+    agregarEstudiante:(nombre, puntosTecnicos, puntosHSE)=>{
+        let estudiante = {
+            nombre: nombre,
+            puntosTecnicos: puntosTecnicos,
+            puntosHSE: puntosHSE
+        }
+        aplicacion.estudiantes.push(estudiante);
+        return estudiante;
+    },
+    mostrar:(estudiante)=> {
+        let fichaEstudiante = `
+            <div class = "estudiante">
+                <h3 class="text-uppercase">${estudiante.nombre}</h3>
+                <strong>Tech Skills:</strong> ${estudiante.puntosTecnicos}%<br>
+                <strong>Life Skills:</strong> ${estudiante.puntosHSE}%<br>
+                <strong>Status:</strong> Active<br>
+            </div>
+        `
+        return fichaEstudiante;
+    },
+    mostrarLista:(estudiantes)=>{
+        return estudiantes.map(mostrar);
+    },
+    estudiantesPromedioalto:()=> {
+        return aplicacion.estudiantes.filter(a=>a.puntosTecnicos>=70 && a.puntosHSE>=70);
+    },
+    reiniciar:()=>{
+        $('#puntosTecnicos').val('');
+        $('#puntosHSE').val('');
+        $("#nombre").val('');
+        $("#nombre").next().css('visibility','hidden');
+        $('#range').html(50);
+        $('#range2').html(50);
+        $('#agregar').removeAttr('data-dismiss');
+    },
+    eventoAgregar:()=>{
+        let nombre = $('#nombre').val();
+        let puntosTecnicos = $("#puntosTecnicos").val();
+        let puntosHSE = $("#puntosHSE").val();
+        if(nombre==''){
+            $("#nombre").next().css('visibility','visible');
+        }else{
+            $('#agregar').attr('data-dismiss',"modal");
+            let estudiante = aplicacion.agregarEstudiante(nombre, puntosTecnicos, puntosHSE);
+            $("#fichas").html(aplicacion.mostrar(estudiante));
+        }
+    },
+    eventoMostrar:()=>{
+        $("#fichas").html(aplicacion.mostrarLista(aplicacion.estudiantes));
+    },
+    eventoMostrarEmpleables:()=> {
+        let empleables = aplicacion.estudiantesPromedioalto();
+        $('#fichas').html(aplicacion.mostrarLista(empleables));
+    },
+    eventoEliminar:()=> {
+        aplicacion.estudiantesPromedioalto();
+        $('#fichas').html(aplicacion.mostrarLista(estudiantes));
+    },
+    iniciar:()=> {
+        $("#agregar").click(aplicacion.eventoAgregar);
+        $('#agregando').click(aplicacion.reiniciar);
+        $('#mostrar').click(aplicacion.eventoMostrar);
+        $('#empleables').click(aplicacion.eventoMostrarEmpleables);
+        $('#eliminadas').click(aplicacion.eventoEliminar);
     }
 }
- //la mayoria son funciones puras
-function agregarEstudiante(nombre, puntosTecnicos, puntosHSE){
-    let estudiante = new Estudiante(nombre, puntosTecnicos, puntosHSE);
-    estudiantes.push(estudiante);
-    return estudiante;
-}
-
-function mostrar(estudiante) {
-    let fichaEstudiante = `
-        <div class = "estudiante">
-            <h3 class="text-uppercase">${estudiante.nombre}</h3>
-            <strong>Tech Skills:</strong> ${estudiante.puntosTecnicos}%<br>
-            <strong>Life Skills:</strong> ${estudiante.puntosHSE}%<br>
-            <strong>Status:</strong> Active<br>
-        </div>
-    `
-    return fichaEstudiante;
-}
-
-function mostrarLista(estudiantes){
-    return estudiantes.map(mostrar);
-}
-
-function estudiantesPromedioalto(estudiantes) {
-    return estudiantes.filter(a=>a.puntosTecnicos>=70 && a.puntosHSE>=70);
-}
-
-function reiniciar() {
-    $('#puntosTecnicos').val('');
-    $('#puntosHSE').val('');
-    $("#nombre").val('');
-    $("#nombre").next().css('visibility','hidden');
-    $('#range').html(50);
-    $('#range2').html(50);
-    $('#agregar').removeAttr('data-dismiss');
-}
-function eventoAgregar (){
-    let nombre = $('#nombre').val();
-    let puntosTecnicos = $("#puntosTecnicos").val();
-    let puntosHSE = $("#puntosHSE").val();
-    let estudiante = agregarEstudiante(nombre, puntosTecnicos, puntosHSE);
-
-    if(nombre==''){
-        $("#nombre").next().css('visibility','visible');
-    }else{
-        $('#agregar').attr('data-dismiss',"modal");
-        $("#fichas").html(mostrar(estudiante));
-    }
-}
-
-function eventoMostrar(){
-    $("#fichas").html(mostrarLista(estudiantes));
-}
-
-function eventoMostrarEmpleables() {
-    let empleables = estudiantesPromedioalto(estudiantes);
-    $('#fichas').html(mostrarLista(empleables));
-}
-
-function eventoEliminar() {
-    let eliminar = estudiantesPromedioalto(estudiantes);
-    estudiantes=eliminar;
-    $('#fichas').html(mostrarLista(estudiantes));
-}
-function eventos(params) {
-    $("#agregar").click(eventoAgregar);
-    $('#agregando').click(reiniciar);
-    $('#mostrar').click(eventoMostrar);
-    $('#empleables').click(eventoMostrarEmpleables);
-    $('#eliminadas').click(eventoEliminar);
-}
+ $(document).ready(()=>{
+    aplicacion.iniciar();
+ })
